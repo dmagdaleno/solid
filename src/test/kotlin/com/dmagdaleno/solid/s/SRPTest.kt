82fe54aafb2a647a10e4463009b5192d2cfa7544
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-04-21 15:50:56 -0300

    test single responsability principle

---
+++ src/test/kotlin/com/dmagdaleno/solid/s/SRPTest.kt
@@ -0,0 +1,43 @@
+package com.dmagdaleno.solid.s
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class SRPTest {
+
+    @Test
+    fun shouldDiscount20PerCent() {
+        val salary = 3001.0
+        val employee = Employee("Employee 1", salary, Title.DEVELOPER)
+        val discountedSalary = employee.calculateTax()
+
+        assertThat(discountedSalary).isEqualTo(salary * 0.8)
+    }
+
+    @Test
+    fun shouldDiscount10PerCent() {
+        val salary = 3000.0
+        val employee = Employee("Employee 1", salary, Title.DEVELOPER)
+        val discountedSalary = employee.calculateTax()
+
+        assertThat(discountedSalary).isEqualTo(salary * 0.9)
+    }
+
+    @Test
+    fun shouldDiscount25PerCent() {
+        val salary = 2001.0
+        val employee = Employee("Employee 1", salary, Title.TESTER)
+        val discountedSalary = employee.calculateTax()
+
+        assertThat(discountedSalary).isEqualTo(salary * 0.75)
+    }
+
+    @Test
+    fun shouldDiscount15PerCent() {
+        val salary = 2000.0
+        val employee = Employee("Employee 1", salary, Title.TESTER)
+        val discountedSalary = employee.calculateTax()
+
+        assertThat(discountedSalary).isEqualTo(salary * 0.85)
+    }
+}
