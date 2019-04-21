f4e9455e586e4302e0684c46f8e4558a72fe070a
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-04-21 15:13:23 -0300

    initial commit

---
+++ src/test/kotlin/com/dmagdaleno/solid/SolidApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.dmagdaleno.solid
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SolidApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
