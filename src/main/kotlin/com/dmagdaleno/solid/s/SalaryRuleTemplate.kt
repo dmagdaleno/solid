52034d7a2e455bd6d913a2595f08ac2219964674
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-04-21 15:43:04 -0300

    implements Salary Rule and creates a template algorithm

---
+++ src/main/kotlin/com/dmagdaleno/solid/s/SalaryRuleTemplate.kt
@@ -0,0 +1,19 @@
+package com.dmagdaleno.solid.s
+
+abstract class SalaryRuleTemplate: SalaryRule {
+
+    override fun calculate(employee: Employee): Double {
+        return if(employee.salary > salaryThreshold()) {
+            employee.salary * maxDiscount()
+        }
+        else {
+            employee.salary * minDiscount()
+        }
+    }
+
+    abstract fun salaryThreshold(): Double
+
+    abstract fun maxDiscount(): Double
+
+    abstract fun minDiscount(): Double
+}
